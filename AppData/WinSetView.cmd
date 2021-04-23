50591e21a0952318b39e1fefc51097b82a612700
LesFerch <79026235+LesFerch@users.noreply.github.com>
2021-04-23 12:07:56 -0400

    Add files via upload

---
+++ AppData/WinSetView.cmd
@@ -0,0 +1,13 @@
+@Echo Off
+
+if exist ".\WinSetViewParams.txt" goto OK
+echo:
+echo Usage:- Run WinSetView.hta to create or update WinSetViewParams.txt
+echo         Or drop a previously saved/renamed MyParams.txt on this cmd
+echo:
+pause & exit
+:OK
+Set /p Params=<".\WinSetViewParams.txt"
+if exist ".\%~n1.txt" Set /p Params=<".\%~n1.txt"
+
+Powershell.exe -ExecutionPolicy Bypass ..\WinSetView.ps1 %Params%
