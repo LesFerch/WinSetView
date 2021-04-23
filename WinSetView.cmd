a2c931322f8f60aef64a82a5d3b6c11abfd30696
LesFerch <79026235+LesFerch@users.noreply.github.com>
2021-04-23 12:07:39 -0400

    Delete WinSetView.cmd

--- WinSetView.cmd
+++
@@ -1,7 +0,0 @@
-@Echo Off
-
-if not exist ".\AppData\WinSetViewParams.txt" echo Usage:- ^
-Run WinSetView.hta to create or update WinSetViewParams.txt & pause & exit
-
-Set /p Params=<".\AppData\WinSetViewParams.txt"
-Powershell.exe -ExecutionPolicy Bypass .\WinSetView.ps1 %Params%
